7d1fce8dac62f3f1335ffe6439550acda148b15a
AlexisBssn <alexis.buisson@hotmail.com>
2016-02-17 11:07:37 -0500

    modified:   app/src/main/java/com/alexis/colval/giftrain/Homepage.java 	modified:   app/src/main/java/com/alexis/colval/giftrain/Profile.java 	modified:   app/src/main/res/layout/activity_profile.xml

--- app/src/main/java/com/alexis/colval/giftrain/Homepage.java
+++ app/src/main/java/com/alexis/colval/giftrain/Homepage.java
@@ -3,6 +3,7 @@ package com.alexis.colval.giftrain;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Homepage extends AppCompatActivity {
 
@@ -12,22 +13,22 @@ public class Homepage extends AppCompatActivity {
         setContentView(R.layout.activity_homepage);
     }
 
-    public void OnClickLogin(){
+    public void OnClickLogin(View v){
         Intent intent = new Intent(this, Login.class);
         startActivity(intent);
     }
 
-    public void onClickProfile(){
+    public void onClickProfile(View v){
         Intent intent = new Intent(this, Profile.class);
         startActivity(intent);
     }
 
-    public void onClickWishlist(){
+    public void onClickWishlist(View v){
         Intent intent = new Intent(this, Wishlist.class);
         startActivity(intent);
     }
 
-    public void onClickGroupSearch(){
+    public void onClickGroupSearch(View v){
         Intent intent = new Intent(this, GroupSearch.class);
         startActivity(intent);
     }
--- app/src/main/java/com/alexis/colval/giftrain/Profile.java
+++ app/src/main/java/com/alexis/colval/giftrain/Profile.java
@@ -2,12 +2,38 @@ package com.alexis.colval.giftrain;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.jar.Attributes;
 
 public class Profile extends AppCompatActivity {
+
+    //temporary no-db variables
+    String firstName="Alexis",lastName="Buisson",email="alexis.buisson@hotmail.com",region="Montérégie";
+    Date birthDate = new Date(1996 + 1900, 3, 27);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        TextView title = (TextView)findViewById(R.id.ProfileTitle);
+        title.setText(firstName + "'s Profile");
+        populate(firstName,lastName,email,region,birthDate.toString());
+    }
+    private void populate(String fN, String lN, String email, String region, String bd){
+        TextView tv = (TextView)findViewById(R.id.firstName);
+        tv.setText(fN);
+        tv = (TextView)findViewById(R.id.lastName);
+        tv.setText(lN);
+        tv = (TextView)findViewById(R.id.email);
+        tv.setText(email);
+        tv = (TextView)findViewById(R.id.region);
+        tv.setText(region);
+        tv = (TextView)findViewById(R.id.birthDate);
+        tv.setText(bd.toString());
     }
 }
